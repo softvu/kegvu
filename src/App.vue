678e59eb91e7ceb85123aff7018a45a549133c93
Dale Matthews <dale.matthews@softvu.com>
2018-08-16 17:16:51 -0500

    Create a basic working example with v-autocomplete and untappd API

--- src/App.vue
+++ src/App.vue
@@ -2,9 +2,23 @@
   <v-app>
     <v-toolbar app d-flex align-center>
       <v-toolbar-title>KegVu</v-toolbar-title>
+
+      <v-autocomplete
+        @input="addBeer"
+        :items="items"
+        :loading="isLoading"
+        :search-input.sync="search"
+        hide-no-data
+        hide-selected
+        item-text="Description"
+        placeholder="Search Untappd Beers"
+        prepend-icon="mdi-database-search"
+        return-object
+      ></v-autocomplete>
+      <v-spacer></v-spacer>
     </v-toolbar>
     <v-content>
-      <HelloWorld/>
+      <BeerDisplay :beers="beers"/>
     </v-content>
     <v-footer fixed app>
       <span>&copy; {{ (new Date()).getFullYear() }}</span>
@@ -13,17 +27,66 @@
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld';
+import axios from 'axios';
+import debounce from 'lodash/debounce';
+import { UNTAPPD } from './config';
+import BeerDisplay from './components/BeerDisplay';
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    BeerDisplay,
   },
   data () {
     return {
-      items: [],
+      beers: [],
+      descriptionLimit: 60,
+      queryResults: [],
+      isLoading: false,
+      search: null,
     };
+  },
+  computed: {
+    items () {
+      return this.queryResults.map(data => {
+        const Description = `${data.beer.beer_name} by ${data.brewery.brewery_name}`;
+        // const bid = data.beer.bid; // move beer ID up so v-autcomplete can use it as an item value
+
+        return Object.assign({}, data, { Description });
+      })
+    }
+  },
+  watch: {
+    search() {
+      this.queryUntappd();
+    },
+  },
+  created() {
+    // TODO first call works but subsequent calls not working...?
+    // we only get 100 untappd API calls per hour ;(
+    this.queryUntappd = debounce(this.queryUntappd, 700, { 'maxWait': 3500 });
+  },
+  methods: {
+    queryUntappd() {
+      // Items have already been loaded
+      if (this.items.length > 0) return;
+
+      this.isLoading = true;
+
+      console.log('querying untappd...');
+      // Lazily load input items
+      axios.get(`https://api.untappd.com/v4/search/beer?q=${this.search}&client_id=${UNTAPPD.client_id}&client_secret=${UNTAPPD.client_secret}`)
+        .then(res => {
+          this.queryResults = (res.data && res.data.response && res.data.response.beers && res.data.response.beers.items) || []; // >_>
+        })
+        .catch(err => {
+          console.error(err);
+        })
+        .finally(() => (this.isLoading = false));
+    },
+    addBeer(data) {
+      this.beers.push(data);
+    },
   },
 }
 </script>
---
+++ src/components/BeerDisplay.vue
@@ -0,0 +1,37 @@
+<template>
+  <v-container fluid>
+    <v-slide-y-transition mode="out-in">
+      <v-layout row align-center>
+        <v-card class="ma-4" v-for="data in beers">
+          <!-- TODO there's a better image if you use the beer info API endpoint -->
+          <v-card-media :src="data.beer.beer_label" height="200px"></v-card-media>
+
+          <v-card-title primary-title>
+            <div>
+              <h3 class="headline mb-0">{{ data.beer.beer_name }}</h3>
+              <div>Made by {{ data.brewery.brewery_name }}</div>
+            </div>
+          </v-card-title>
+
+          <v-card-actions>
+            <v-btn flat color="orange">Share</v-btn>
+            <v-btn flat color="orange">Explore</v-btn>
+          </v-card-actions>
+        </v-card>
+      </v-layout>
+    </v-slide-y-transition>
+  </v-container>
+</template>
+
+<script>
+export default {
+  name: 'BeerDisplay',
+  props: {
+    beers: Array,
+  }
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+</style>
