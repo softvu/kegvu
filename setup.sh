19eefa06ff3127058c570350aed5387c9b7cc480
Brian Hann <brian.hann@gmail.com>
2018-11-16 17:04:38 -0600

    Old changes

--- setup.sh
+++ setup.sh
@@ -3,14 +3,18 @@
 # Install node
 curl -sL https://deb.nodesource.com/setup_8.x | sudo -E bash -
 sudo apt-get install -y nodejs
+sudo apt-get install -y redis-server
 
-npm install -g pm2
+sudo npm install -g pm2
 
 npm install
 
 (cd api && npm install)
 
 # Install python requirements
-pip3 install -r api/requirements.txt
+pip3 install -r pi/requirements.txt
 
+sudo pm2 startup
 pm2 start ecosystem.config.yml
+
+echo "Make sure to run deploy from your dev machine"
